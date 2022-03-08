f9d11ca5a24e6f12685b80a219ec6489247d0af3
Shrestha-Dipesh <shrestha.dipesh1999@gmail.com>
2022-03-08 20:18:27 +0545

    Implemented player movement and jump mechanism

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float moveForce = 7f;
+
+    [SerializeField]
+    private float jumpForce = 7f;
+
+    private Rigidbody2D mRigidBody;
+    private float directionX;
+    private bool canJump = true;
+
+    private void Awake()
+    {
+        mRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        MovePlayer();
+        JumpPlayer();
+    }
+
+    private void MovePlayer()
+    {
+        directionX = Input.GetAxisRaw("Horizontal");
+        transform.position += new Vector3 (directionX, 0f, 0f) * moveForce * Time.deltaTime;
+    }
+
+    private void JumpPlayer()
+    {
+        if ((Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W)) && canJump)
+        {
+            canJump = false;
+            mRigidBody.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Platform"))
+        {
+            canJump = true;
+        }
+    }
+}
