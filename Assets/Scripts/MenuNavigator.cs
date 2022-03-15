ca1bc208cd8d401703a1a81fd3d234954968fa78
Shrestha-Dipesh <shrestha.dipesh1999@gmail.com>
2022-03-15 19:26:59 +0545

    Implemented player selection screen with dynamic player image and name

--- Assets/Scripts/MenuNavigator.cs
+++ Assets/Scripts/MenuNavigator.cs
@@ -2,10 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuNavigator : MonoBehaviour
 {
-    public void PlayGame()
+    [SerializeField]
+    private GameObject selectionMenu;
+
+    [SerializeField]
+    private GameObject detailsMenu;
+
+    [SerializeField]
+    private Text playerName;
+
+    [SerializeField]
+    private Image playerImage;
+
+    [SerializeField]
+    private Sprite circleImage;
+
+    [SerializeField]
+    private Sprite squareImage;
+
+    public void SelectCharacter()
     {
         SceneManager.LoadScene("Player Selection");
     }
@@ -13,5 +32,34 @@ public class MenuNavigator : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Level 1");
+    }
+
+    public void DisplaySelectionMenu()
+    {
+        detailsMenu.SetActive(false);
+        selectionMenu.SetActive(true);
+    }
+
+    public void DisplayDetailsMenu()
+    {
+        selectionMenu.SetActive(false);
+        detailsMenu.SetActive(true);
+
+        string buttonClicked = UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name;
+        if (buttonClicked == "Player 1 Button")
+        {
+            playerName.text = "Circle";
+            playerImage.sprite = circleImage;
+        }
+        else if (buttonClicked == "Player 2 Button")
+        {
+            playerName.text = "Square";
+            playerImage.sprite = squareImage;
+        }
     }
 }
